2bf306d0e828c5177169c598f6a0b97b79a5cde3
harpreet1313chahal-ux <harpreet1313chahal@gmail.com>
2026-07-09 10:27:36 -0500

    Add sorting to books page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,6 +10,19 @@ def index
     books = books.where(category_id: params[:category_id])
   end
 
+  if params[:sort].present?
+    case params[:sort]
+    when "title_asc"
+      books = books.order(title: :asc)
+    when "title_desc"
+      books = books.order(title: :desc)
+    when "rating_desc"
+      books = books.order(rating: :desc)
+    when "rating_asc"
+      books = books.order(rating: :asc)
+    end
+  end
+
   @categories = Category.order(:name)
 
   @pagy, @books = pagy(books, limit: 20)
