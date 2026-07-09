43c343e37f66ff592a115acf097e4568e2ec5eac
harpreet1313chahal-ux <harpreet1313chahal@gmail.com>
2026-07-09 09:32:14 -0500

    Clean up seed script

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,73 +1,19 @@
-require "csv"
+book_title = row["Name"].to_s.strip
+next if book_title.blank?
 
-puts "Cleaning database..."
+count += 1
+puts "Imported #{count} books..." if count % 1000 == 0
 
-Book.destroy_all
-Author.destroy_all
-Publisher.destroy_all
-Category.destroy_all
-
-puts "Importing books..."
-
-csv_file = Rails.root.join("db", "data", "books.csv")
-
-CSV.foreach(csv_file, headers: true) do |row|
-
-  author_name = row["Authors"].to_s.strip
-  author_name = "Unknown Author" if author_name.blank?
-
-  author = Author.find_or_create_by!(
-    name: author_name
-  )
-
-  publisher_name = row["Publisher"].to_s.strip
-  publisher_name = "Unknown Publisher" if publisher_name.blank?
-
-  publisher = Publisher.find_or_create_by!(
-    name: publisher_name
-  )
-
-  author_name = row["Authors"].to_s.downcase
-
-  category_name =
-    case author_name
-    when /rowling/
-      "Fantasy"
-    when /tolkien/
-      "Fantasy"
-    when /douglas adams/
-      "Science Fiction"
-    when /bill bryson/
-      "Travel"
-    when /stephen king/
-      "Horror"
-    when /dan brown/
-      "Thriller"
-    when /agatha christie/
-      "Mystery"
-    when /dr\.? seuss/
-      "Children"
-    else
-      "General"
-    end
-
-  category = Category.find_or_create_by!(
-    name: category_name
-  )
-
-  book_title = row["Name"].to_s.strip
-  next if book_title.blank?
-
-  Book.create!(
-    title: book_title,
-    isbn: row["ISBN"].to_s,
-    language: row["Language"],
-    pages: row["pagesNumber"].to_i,
-    rating: row["Rating"].to_f,
-    author: author,
-    publisher: publisher,
-    category: category
-  )
+Book.create!(
+  title: book_title,
+  isbn: row["ISBN"].to_s,
+  language: row["Language"],
+  pages: row["pagesNumber"].to_i,
+  rating: row["Rating"].to_f,
+  author: author,
+  publisher: publisher,
+  category: category
+)
 end
 
 puts "Done!"
