affe2dcf7986676a91aed323d15f45022cc5df3f
harpreet1313chahal-ux <harpreet1313chahal@gmail.com>
2026-07-09 10:36:19 -0500

    Improve books search and sorting

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,32 +1,28 @@
 class BooksController < ApplicationController
-def index
-  books = Book.includes(:author, :publisher, :category)
+  def index
+    books = Book.includes(:author, :publisher, :category)
 
-  if params[:search].present?
-    books = books.where("title LIKE ?", "%#{params[:search]}%")
-  end
+    if params[:search].present?
+      books = books.where("title LIKE ?", "%#{params[:search]}%")
+    end
 
-  if params[:category_id].present?
-    books = books.where(category_id: params[:category_id])
-  end
+    if params[:category_id].present?
+      books = books.where(category_id: params[:category_id])
+    end
 
-  if params[:sort].present?
     case params[:sort]
-    when "title_asc"
-      books = books.order(title: :asc)
-    when "title_desc"
-      books = books.order(title: :desc)
+    when "title"
+      books = books.order(:title)
     when "rating_desc"
       books = books.order(rating: :desc)
     when "rating_asc"
       books = books.order(rating: :asc)
     end
+
+    @categories = Category.order(:name)
+
+    @pagy, @books = pagy(books, limit: 20)
   end
-
-  @categories = Category.order(:name)
-
-  @pagy, @books = pagy(books, limit: 20)
-end
 
   def show
     @book = Book.find(params[:id])
